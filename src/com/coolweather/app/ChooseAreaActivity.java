2e5e5b3e0e3d005fb917c85df5500a05037b9885
Tonny <tony@gmail.com>
2016-03-20 10:46:19 +0800

    新增切换城市和手动更新天气的功能

--- src/com/coolweather/app/ChooseAreaActivity.java
+++ src/com/coolweather/app/ChooseAreaActivity.java
@@ -40,6 +40,9 @@ public class ChooseAreaActivity extends Activity{
 	private CoolWeatherDB coolWeatherDB;
 	private List<String> dataList=new ArrayList<String>();
 	
+	//判断是否从WeatherActivity中跳转过来
+	private boolean isFromWeatherActivity;
+	
 	/**
 	 * 省列表
 	 */
@@ -70,8 +73,11 @@ public class ChooseAreaActivity extends Activity{
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		
+		isFromWeatherActivity=getIntent().getBooleanExtra("from_weather_actvity", false);
+		
 		SharedPreferences prefs=PreferenceManager.getDefaultSharedPreferences(this);
-		if(prefs.getBoolean("citySelectedId", false)){
+		//已经选择了城市且不是从且不是从WeatherActivity跳转过来，才会直接跳转到WeatherActivity
+		if(prefs.getBoolean("citySelectedId", false)&&!isFromWeatherActivity){
 			Intent intent=new Intent(this,WeatherActivity.class);
 			startActivity(intent);
 			finish();
@@ -255,6 +261,10 @@ public class ChooseAreaActivity extends Activity{
 		}else if(currentLevel==LEVEL_CITY){
 			queryProvinces();
 		}else{
+			if(isFromWeatherActivity){
+				Intent intent=new Intent(this,WeatherActivity.class);
+				startActivity(intent);
+			}
 			finish();
 		}
 	}
--- src/com/coolweather/app/WeatherActivity.java
+++ src/com/coolweather/app/WeatherActivity.java
@@ -4,16 +4,20 @@ import util.HttpCallbackListener;
 import util.HttpUtil;
 import util.Utility;
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class WeatherActivity extends Activity{
+public class WeatherActivity extends Activity implements OnClickListener{
 	private LinearLayout weatherInfoLayout;
 	
 	private TextView cityNameText;	//用于显示城市名
@@ -21,7 +25,10 @@ public class WeatherActivity extends Activity{
 	private TextView weatherDespText;//用于显示天气描述信息
 	private TextView temp1Text;		//用于显示气温1
 	private TextView temp2Text;		//用于显示气温2
-	private TextView currentDataText;//用于显示当前日期
+	private TextView currentDateText;//用于显示当前日期
+	
+	private Button switchCity;//切换城市按钮
+	private Button refreshWeather;//刷新天气按钮
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
@@ -35,8 +42,15 @@ public class WeatherActivity extends Activity{
 		weatherDespText=(TextView)findViewById(R.id.weather_desp);
 		temp1Text=(TextView)findViewById(R.id.temp1);	
 		temp2Text=(TextView)findViewById(R.id.temp2);	
-		currentDataText=(TextView)findViewById(R.id.current_date);
+		currentDateText=(TextView)findViewById(R.id.current_date);
 		String countyCode=getIntent().getStringExtra("county_code");
+		
+		switchCity=(Button)findViewById(R.id.switch_city);
+		refreshWeather=(Button)findViewById(R.id.refresh_weather);
+		switchCity.setOnClickListener(this);
+		refreshWeather.setOnClickListener(this);
+		
+		
 		if(!TextUtils.isEmpty(countyCode)){
 			//优先级代号时就去查询天气
 			publishText.setText("同步中...");
@@ -48,6 +62,36 @@ public class WeatherActivity extends Activity{
 			showWeather();
 		}
 	}
+	
+	
+	
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch(v.getId()){
+		case R.id.switch_city:
+			Intent intent=new Intent(this,ChooseAreaActivity.class);
+			intent.putExtra("from_weather_activity",true);
+			startActivity(intent);
+			finish();
+			break;
+		case R.id.refresh_weather:
+			publishText.setText("同步中");
+			SharedPreferences prefs=PreferenceManager.getDefaultSharedPreferences(this);
+			String weatherCode=prefs.getString("weather_code","");
+			
+			if(!TextUtils.isEmpty(weatherCode)){
+				//Log.d("WeatherActivity","weather code is not null!!!");
+				queryWeatherInfo(weatherCode);
+				
+			}
+			break;
+		default:
+			break;
+		}
+	}
+	
+	
 	
 	/*
 	 * 查询县级代号所对应的天气代号
@@ -117,7 +161,7 @@ public class WeatherActivity extends Activity{
 		temp2Text.setText(prefs.getString("temp2", ""));
 		weatherDespText.setText(prefs.getString("weather_desp",""));
 		publishText.setText("今天"+prefs.getString("publish_time", "发布"));
-		currentDataText.setText(prefs.getString("current_data", ""));
+		currentDateText.setText(prefs.getString("current_date", ""));
 		weatherInfoLayout.setVisibility(View.VISIBLE);
 		cityNameText.setVisibility(View.VISIBLE);
 		
